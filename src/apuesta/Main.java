092f595c23f391076f6f45a3deacc2ef19e04a02
danielach28 <daniela_chg@hotmail.com>
2024-03-01 19:54:05 +0100

    comentario

---
+++ src/apuesta/Main.java
@@ -0,0 +1,34 @@
+package apuesta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int dinero=0;
+        operativa_apuesta(dinero);
+        
+        
+    }
+
+    private static void operativa_apuesta(int dinero) {
+        Apuesta laApuesta;
+        int mi_dinero;
+        
+
+        laApuesta = new Apuesta(1000, 4, 2);
+        try {
+            System.out.println("Apostando...");
+            laApuesta.apostar(25);
+        } catch (Exception e) {
+            System.out.println("Fallo al realizar la Apuesta");
+        }
+
+        try {
+            System.out.println("Intento cobrar apuesta segun el resultado del partido");
+            laApuesta.cobrar_apuesta(2, 3);
+        } catch (Exception e) {
+            System.out.println("Fallo al cobrar la apuesta");
+        }
+        mi_dinero = laApuesta.getDinero_disp();
+        System.out.println("El dinero que tengo tras las apuestas es " + mi_dinero);
+    }
+}
